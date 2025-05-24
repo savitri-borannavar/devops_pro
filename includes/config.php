8edb64e1396c62542c789379ca627298e2dc3968
savitri-borannavar <131450634+savitri-borannavar@users.noreply.github.com>
2025-05-24 22:45:46 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "food_ordering");
+$host = "db"; // This must match the service name in docker-compose.yml
+$user = "food_user";
+$pass = "food_pass";
+$dbname = "food_order";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
